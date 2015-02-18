45cc0af2bb51d571b7212ab444a0ae39ac1f035f
Andrew Tayman <dretay@gmail.com>
2015-02-18 01:23:25 -0500

    added interceptors

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var factory = require('factory-girl'),
+  var factory = require('factory-girl'),
     Adapter = factory.Adapter;
 
 var BookshelfAdapter = function() {};
 BookshelfAdapter.prototype = new Adapter();
+
 BookshelfAdapter.prototype.build = function(Model, props) {
   return new Model(props);
 };
@@ -13,14 +14,35 @@ BookshelfAdapter.prototype.set = function(props, doc, Model) {
   return doc.set(props);
 };
 BookshelfAdapter.prototype.save = function(doc, Model, cb) {
-  doc.save().nodeify(cb);
+  var that = this;
+  doc.save().then(function(model){
+    for(var x =0;x< that.interceptors['save'].length; x++){
+      that.interceptors['save'][x].call(that,model)
+    }
+    cb(null,model)
+  });
 };
 BookshelfAdapter.prototype.destroy = function(doc, Model, cb) {
   if (!doc.id) return process.nextTick(cb);
-  doc.destroy().nodeify(cb);
+  var that = this;
+  doc.destroy().then(function(model){
+    for(var x =0;x< that.interceptors['destroy'].length; x++){
+      that.interceptors['destroy'][x].call(that,model)
+    }
+    cb(null,model)
+  });
 };
-
+BookshelfAdapter.prototype.addInterceptor = function(type,callback){
+  this.interceptors[type].push(callback);
+};
+BookshelfAdapter.prototype.clearInterceptors = function(type){
+  this.interceptors[type] = [];
+};
 var adapter = new BookshelfAdapter();
+adapter.interceptors = {
+  save: [],
+  destroy: []
+};
 module.exports = function(models) {
   if (models) {
     for (var i = 0; i < models.length; i++) {
